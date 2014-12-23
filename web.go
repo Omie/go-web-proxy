e15e43d7de6db02580b761981a3091e976342757
Omie <intelomkar@gmail.com>
2014-12-23 22:34:34 +0530

    Added basic proxy handler function
    - added a function proxy() to handle paths /p/
    - url scheme should be <hostname>/p/?target=http://blahblah.blahblahblah
    - won't fetch static

--- web.go
+++ web.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"runtime"
@@ -9,6 +11,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/p/", proxy)
 	bind := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
 	fmt.Printf("listening on %s...", bind)
 	err := http.ListenAndServe(bind, nil)
@@ -19,4 +22,30 @@ func main() {
 
 func hello(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(res, "hello, world from %s", runtime.Version())
+}
+
+func proxy(res http.ResponseWriter, req *http.Request) {
+	target := req.URL.Query().Get("target")
+	if len(target) != 0 {
+		client := &http.Client{}
+		r, err := http.NewRequest("GET", target, nil)
+		if err != nil {
+			io.WriteString(res, err.Error())
+			return
+		}
+
+		resp, err := client.Do(r)
+		if err != nil {
+			io.WriteString(res, err.Error())
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			io.WriteString(res, err.Error())
+			return
+		}
+		io.WriteString(res, string(body))
+	}
 }
