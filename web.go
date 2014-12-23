85dc8d94ec75637dd1a06badb7b1a036dd382a0b
Omie <intelomkar@gmail.com>
2014-12-24 01:11:03 +0530

    Update wget based method to make it work
    - register a handle serve newly fetched index.html using FileServer

--- web.go
+++ web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -29,25 +30,28 @@ func hello(res http.ResponseWriter, req *http.Request) {
 }
 
 func proxy(res http.ResponseWriter, req *http.Request) {
+	/* simple function that will fetch single resource and tunnel the response
+	   useful for simple txt or image files
+	*/
 	target := req.URL.Query().Get("target")
 	if len(target) != 0 {
 		client := &http.Client{}
 		r, err := http.NewRequest("GET", target, nil)
 		if err != nil {
-			io.WriteString(res, err.Error())
+			io.WriteString(res, "1001 "+err.Error())
 			return
 		}
 
 		resp, err := client.Do(r)
 		if err != nil {
-			io.WriteString(res, err.Error())
+			io.WriteString(res, "1002 "+err.Error())
 			return
 		}
 		defer resp.Body.Close()
 
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			io.WriteString(res, err.Error())
+			io.WriteString(res, "1003 "+err.Error())
 			return
 		}
 		io.WriteString(res, string(body))
@@ -60,24 +64,32 @@ func proxyWget(res http.ResponseWriter, req *http.Request) {
 		//make temp dir
 		tempPath, err := ioutil.TempDir("", "proxy")
 		if err != nil {
-			io.WriteString(res, err.Error())
+			io.WriteString(res, "1004 "+err.Error())
 			return
 		}
-		fmt.Println(tempPath)
-		os.Chdir(tempPath)
+		os.Chdir(tempPath) //do this to make sure wget will fetch contents in this dir
 
 		_, err = exec.Command("wget", "-p", target).Output()
 		if err != nil {
-			io.WriteString(res, "1003"+err.Error())
+			io.WriteString(res, "1005 "+err.Error())
 			return
 		}
 		u, _ := url.ParseRequestURI(target)
 		h := u.Host
 		fp := filepath.Join(tempPath, h)
-		os.Chdir(fp)
-		fp = filepath.Join(fp, "index.html")
-		http.ServeFile(res, req, fp)
-		//go http.ListenAndServe("0.0.0.0:8080", http.FileServer(http.Dir(fp)))
-		//io.WriteString(res, "done")
+		b := "/" + rand_str(15) + "/"
+		http.Handle(b, http.StripPrefix(b, http.FileServer(http.Dir(fp))))
+		http.Redirect(res, req, b, 302)
 	}
+}
+
+func rand_str(str_size int) string {
+	//https://devpy.wordpress.com/2013/10/24/create-random-string-in-golang/
+	alphanum := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, str_size)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return string(bytes)
 }
